f79b89c0be3d5159670d3898f786cc7caf0fb892
liyechen <gliyechen@hotmail.com>
2019-07-22 18:56:29 +0800

    count-and-say

---
+++ count-and-say.cpp
@@ -0,0 +1,73 @@
+/* Count and Say
+ * 
+ * [Easy] [AC:41.1% 297.7K of 724K] [filetype:cpp]
+ * 
+ * The count-and-say sequence is the sequence of integers with the first five
+ * terms as following:
+ * 
+ * 1.     1
+ * 
+ * 2.     11
+ * 
+ * 3.     21
+ * 
+ * 4.     1211
+ * 
+ * 5.     111221
+ * 
+ * 1 is read off as "one 1" or 11.
+ * 
+ * 11 is read off as "two 1s" or 21.
+ * 
+ * 21 is read off as "one 2, then one 1" or 1211.
+ * 
+ * Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the
+ * count-and-say sequence.
+ * 
+ * Note: Each term of the sequence of integers will be represented as a string.
+ * 
+ * Example 1:
+ * 
+ * Input: 1
+ * 
+ * Output: "1"
+ * 
+ * Example 2:
+ * 
+ * Input: 4
+ * 
+ * Output: "1211"
+ * 
+ * [End of Description] */
+class Solution {
+public:
+    string countAndSay(int n) {
+        string ans = "1";
+        while (n > 1) {
+            ans = nextSay(ans);
+            n--;
+        }
+        return ans;
+    }
+
+private:
+    string nextSay(string str) {
+        char ch = str[0];
+        int chIdx = 1;
+        int count = 1;
+        string res = "";
+        while(chIdx < str.size()) {
+            if (str[chIdx] != ch) {
+                res += to_string(count) + ch;
+                ch = str[chIdx];
+                count = 1;
+            } else {
+                count++;
+            }
+            chIdx++;
+        }
+        res += to_string(count) + ch;
+        return res;
+    }
+};
+
