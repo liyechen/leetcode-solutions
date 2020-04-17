9a898923d422855f9f2eecae43c1cf2b9dd351e4
liyechen <gliyechen@hotmail.com>
2020-04-17 17:26:45 +0800

    95

--- 95.unique-binary-search-trees-ii.cpp
+++ 95.unique-binary-search-trees-ii.cpp
@@ -1,10 +1,3 @@
-/*
- * @lc app=leetcode id=95 lang=cpp
- *
- * [95] Unique Binary Search Trees II
- */
-
-// @lc code=start
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -15,12 +8,36 @@
  * };
  */
 class Solution {
+    
+    // generate treenodes from left to right
+    vector<TreeNode*> generator(int left, int right) {
+        // break
+        vector<TreeNode*> res;
+        if (right < left) {
+            res.push_back(NULL);
+            return res;
+        }
+        
+        // pick i as root
+        for (int i = left; i <= right; i++) {
+            vector<TreeNode*> lefts = generator(left, i - 1);
+            vector<TreeNode*> rights = generator(i + 1, right);
+            for (const auto& leftNode : lefts) {
+                for (const auto& rightNode : rights) {
+                    TreeNode* r = new TreeNode(i);
+                    r->left = leftNode;
+                    r->right = rightNode;
+                    res.push_back(r);
+                }
+            }
+        }
+        
+        return res;
+    }
+    
 public:
     vector<TreeNode*> generateTrees(int n) {
-        vector<TreeNode*> ans;
-
-        return ans;
+        if (n == 0) return vector<TreeNode*>(0);
+        return generator(1, n);
     }
 };
-// @lc code=end
-
